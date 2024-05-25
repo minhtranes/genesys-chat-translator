d89a2e2c8c8310806b3deab622ca8a3d0308feac
Minh Trần <wisekingtran@gmail.com>
2024-05-25 11:35:11 +0700

    Update config

--- docs/scripts/config.js
+++ docs/scripts/config.js
@@ -1,6 +1,6 @@
 export default {
     // Token Implicit Grant Client ID
-    clientID: 'your-client-id-here',
+    clientID: '0b922f71-bc8c-4f28-9a2c-5652e948f151',
 
     redirectUri: 'https://localhost/',
 
